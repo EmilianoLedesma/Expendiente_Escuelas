53c84372603676b418322cdd55f31d7a5ced01e9
Emiliano Ledesma <emilianoledesmaledesma@outlook.com>
2026-09-09 09:55:22 -0600

    Gate the Filament admin panel behind a spatie sedeq role

--- app-laravel/app/Models/User.php
+++ app-laravel/app/Models/User.php
@@ -5,19 +5,22 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Models\Solicitante;
 use Database\Factories\UserFactory;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, HasRoles, Notifiable;
 
     /**
      * Get the attributes that should be cast.
@@ -35,5 +38,10 @@ class User extends Authenticatable
     public function solicitante(): HasOne
     {
         return $this->hasOne(Solicitante::class);
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->hasRole('sedeq');
     }
 }
--- app-laravel/database/seeders/DatabaseSeeder.php
+++ app-laravel/database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
 
+        $this->call(RolesSeeder::class);
         $this->call(CatalogoMinimoSeeder::class);
         $this->call(PasosCapturaSeeder::class);
         $this->call(AsignaturasSeeder::class);
